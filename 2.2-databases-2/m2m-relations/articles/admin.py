49ff0faf6c818bfda4a132b86162f75fa286afeb
Petr <petr_mameev@mail.ru>
2023-05-15 23:09:59 +0700

    Второе задание выполнено и отформатированно

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -4,16 +4,17 @@ from django.forms import BaseInlineFormSet
 
 from .models import Article, Tag, Scope
 
+
 class ScopeInlineFormset(BaseInlineFormSet):
     def clean(self):
         main_count = 0
         for form in self.forms:
             if form.cleaned_data:
-                main_count += int(form.cleaned_data['is_main'])
+                main_count += int(form.cleaned_data["is_main"])
         if main_count == 0:
-            raise ValidationError('Укажите основной раздел')
+            raise ValidationError("Укажите основной раздел")
         elif main_count > 1:
-            raise ValidationError('Допускается только один основной раздел')
+            raise ValidationError("Допускается только один основной раздел")
         return super().clean()
 
 
@@ -21,13 +22,23 @@ class ScopeInLine(admin.TabularInline):
     model = Scope
     formset = ScopeInlineFormset
 
+
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ['title', 'published_at', ]
-    list_filter = ['title', 'published_at', ]
-    inlines = [ScopeInLine, ]
+    list_display = [
+        "title",
+        "published_at",
+    ]
+    list_filter = [
+        "title",
+        "published_at",
+    ]
+    inlines = [
+        ScopeInLine,
+    ]
+
 
 @admin.register(Tag)
 class Tag(admin.ModelAdmin):
-    list_display = ['name']
-    list_filter = ['name']
+    list_display = ["name"]
+    list_filter = ["name"]
--- 2.2-databases-2/m2m-relations/articles/views.py
+++ 2.2-databases-2/m2m-relations/articles/views.py
@@ -4,7 +4,7 @@ from articles.models import Article
 
 
 def articles_list(request):
-    template = 'articles/news.html'
+    template = "articles/news.html"
     articles = Article.objects.all()
-    context = {'object_list': articles.order_by('-published_at')}
+    context = {"object_list": articles.order_by("-published_at")}
     return render(request, template, context)
