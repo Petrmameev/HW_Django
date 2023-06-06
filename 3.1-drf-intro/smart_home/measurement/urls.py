86ec626d3a36ace52cd45a527a2bfa6983c9d39c
Petrmameev <petr_mameev@mail.ru>
2023-06-06 21:54:19 +0700

    Выполнил задание 3.1

--- 3.1-drf-intro/smart_home/measurement/urls.py
+++ 3.1-drf-intro/smart_home/measurement/urls.py
@@ -5,6 +5,6 @@ from .views import SensorView, MasurementView, SensorsView
 
 urlpatterns = [
     path("sensors/", SensorsView.as_view()),
-    path("sensors/<pk>", SensorView.as_view()),
+    path("sensors/<id>", SensorView.as_view()),
     path("measurements/", MasurementView.as_view()),
 ]
--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,44 +1,28 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView
-from rest_framework.response import Response
+from rest_framework.generics import (
+    CreateAPIView,
+    RetrieveUpdateAPIView,
+    ListCreateAPIView,
+)
 
 from .models import Sensor, Measurement
 from .serializers import (
     SensorDetailSerializer,
     SensorSerializer,
-    MeasurementSerializer,
     MeasurementDetailSerializer,
 )
 
 
-class SensorsView(ListAPIView):
+class SensorsView(ListCreateAPIView):
     queryset = Sensor.objects.all()
     serializer_class = SensorSerializer
 
-    def post(self, request):
-        Sensor.objects.create(
-            name=request.POST.get("name"), description=request.POST.get("description")
-        )
-        return Response({"status": "ok"})
 
-
-class SensorView(RetrieveAPIView):
+class SensorView(RetrieveUpdateAPIView):
     queryset = Sensor.objects.all()
     serializer_class = SensorDetailSerializer
-
-    def patch(self, request, pk):
-        sensor = Sensor.object.get(id=pk)
-        sensor.description = request.data["description"]
-        sensor.save()
-        return Response({"status": "ok"})
+    lookup_field = "id"
 
 
-class MasurementView(ListAPIView):
+class MasurementView(CreateAPIView):
     queryset = Measurement.objects.all()
     serializer_class = MeasurementDetailSerializer
-
-    def post(self, request):
-        Measurement.objects.create(
-            sensor_id=request.POST.get("sensor"),
-            temperature=request.POST.get("temperature"),
-        )
-        return Response({"status": "ok"})
