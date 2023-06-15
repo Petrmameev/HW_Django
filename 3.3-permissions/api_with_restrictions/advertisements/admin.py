b760b210645d9b45cf9fab77110477aca56eda33
Petrmameev <petr_mameev@mail.ru>
2023-06-15 23:07:08 +0700

    Выполнил задание 3.3

--- 3.3-permissions/api_with_restrictions/advertisements/admin.py
+++ 3.3-permissions/api_with_restrictions/advertisements/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Advertisement
 
-# Register your models here.
+@admin.register(Advertisement)
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'creator',
+                  'status', 'created_at', 'updated_at']
+
